9145885d49f97810819cd07a9df130347d67a1af
Carlos Berti <carlosberti182@gmail.com>
2020-10-20 00:30:50 -0300

    fix: adjust HttpStatusCode typo

--- src/data/useCases/authentication/remoteAuthentication.ts
+++ src/data/useCases/authentication/remoteAuthentication.ts
@@ -21,7 +21,7 @@ class RemoteAuthentication implements Authentication {
     switch (httpResponse.statusCode) {
       case HttpStatusCode.ok:
         return httpResponse.body
-      case HttpStatusCode.unathorized:
+      case HttpStatusCode.unauthorized:
         throw new InvalidCredentialsError()
       default:
         throw new UnexpectedError()
